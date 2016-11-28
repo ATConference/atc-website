87abde45051b6ad06479ba5818d22ca9256ab22e
Jaewon Seo <byeol@hotmail.co.kr>
2016-11-28 14:00:00 +0900

    chore(pages): change image of the locations page

--- src/app/pages/locations/locations.component.ts
+++ src/app/pages/locations/locations.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class LocationsComponent {
   // tslint:disable-next-line:max-line-length
-  imageUrl = '/assets/xavier.jpg';
+  imageUrl = '/assets/locations.jpg';
   mapsUrl = 'https://www.google.com/maps/embed/v1/place?q=%EC%84%9C%EA%B0%95%EB%8C%80%ED%95%99%EA%B5%90%20art%26technology&key=AIzaSyDm6fKakzwOP7pip3jiGKmyUKTD5Eulj_8';
 }
