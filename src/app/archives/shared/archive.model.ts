e630ad2bbf115a834e948d975ceff61ac0e8c066
Jaewon Seo <byeol@hotmail.co.kr>
2016-11-29 20:00:00 +0900

    feat(archives): display the name of the director at archive detail page

--- src/app/archives/shared/archive.model.ts
+++ src/app/archives/shared/archive.model.ts
@@ -1,8 +1,11 @@
+import { Participant } from '../../participants/shared/participant.model';
+
 export class Archive {
   id: number;
   name: Object;
   description: Object;
   year: number;
+  director: Participant;
   thumbImage: string;
   images: string[];
 }
